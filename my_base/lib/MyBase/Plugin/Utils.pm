46fcf7abd8fc54ce7169b662117e0798d795c5b8
flashersha <hotjobs@yandex.ru>
2017-05-15 10:29:51 +0300

    Update Utils.pm

--- my_base/lib/MyBase/Plugin/Utils.pm
+++ my_base/lib/MyBase/Plugin/Utils.pm
@@ -3,44 +3,46 @@ use Mojo::Base 'Mojolicious::Plugin';
 
 sub register {
 	my ( $self, $app ) = @_;
-	
-    $app->helper( limit_offset => sub {
-        my ($c)    = @_;
-        my $offset =
-            $c->{'args'}->{'current'}
-                || $c->config->{'settins'}->{'page'}->{'rows'};
-        
-        my $limit =
-            $c->{'args'}->{'rowCount'}
-                || 1;
+
+	$app->helper( limit_offset => sub {
+		my ($c)    = @_;
+		my $offset =
+			$c->{'args'}->{'current'}
+			|| $c->config->{'settins'}->{'page'}->{'rows'};
 		
-        $offset = ($offset - 1) * $limit;
-        
-        return $limit, $offset;
-    } );
-    
-    $app->helper( order_by => sub {
-        my ($c)   = @_;
-        my $order = [];
-        my $set   = {
-            desc => '-',
-            asc  => '+',
-        };
-        
-        for my $key ( keys %{ $c->{'args'} } ){
-            if( $key =~ /^sort\[(\w+)\]$/i ){
-                push @{$order},  
-                    $set->{lc( $c->{'args'}->{$key} )}
-                    . $1
-                ;
-                
-                last;
-            }
-        }        
-        
-        return $order;
-    } );    
+		my $limit =
+			$c->{'args'}->{'rowCount'}
+			|| 1;
+		
+		$offset = ($offset - 1) * $limit;
+		
+		return $limit, $offset;
+	} );
 	
+	$app->helper( order_by => sub {
+		my ($c)   = @_;
+		my $order = [];
+		my $set   = {
+			desc => '-',
+			asc  => '+',
+		};
+	
+		for my $key ( keys %{ $c->{'args'} } ){
+			if( $key =~ /^sort\[(\w+)\]$/i ){
+				push @{$order},  
+					$set->{lc( $c->{'args'}->{$key} )}
+					. $1
+					;
+		
+				last;
+			}
+		}        
+	
+		return $order;
+	} );
+	
+	return 1;
+
 }
-    
+
 1;
